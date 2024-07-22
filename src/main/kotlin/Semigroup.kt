258521e38f8a06433e67e823bc0f76281978afca
Mike Sperber <sperber@deinprogramm.de>
2024-07-22 16:08:07 +0200

    Update.

--- src/main/kotlin/Semigroup.kt
+++ src/main/kotlin/Semigroup.kt
@@ -1,7 +1,11 @@
 interface Semigroup<T> {
+    // combine(combine(a, b), c) = combine(a, combine(b, c))
     fun combine(t1: T, t2: T): T
 }
 
-fun <A> listSemigroup(): Semigroup<List<A>> {
-
+class ListSemigroup<A>: Semigroup<List<A>> {
+    override fun combine(t1: List<A>, t2: List<A>): List<A> =
+        append(t1, t2)
 }
+
+fun <A> listSemigroup() = ListSemigroup<A>()
