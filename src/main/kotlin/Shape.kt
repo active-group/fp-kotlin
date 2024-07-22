7b8d57aef33feb4201c3c8ade624d3828ba37c93
Mike Sperber <sperber@deinprogramm.de>
2024-07-22 11:18:44 +0200

    Update.

---
+++ src/main/kotlin/Shape.kt
@@ -0,0 +1,10 @@
+// "Geometric Region Server"
+
+// Eine (geometrische) Figur ist eins der folgenden:
+// - ein Kreis
+// - ein Quadrat
+// - eine Überlagerung zweier geometrischer Figuren
+
+// 1. Datenmodellierung
+// 2. Funktion, die ermittelt, ob ein gegebener Punkt
+//    innerhalb oder außerhalb einer Figur ist
