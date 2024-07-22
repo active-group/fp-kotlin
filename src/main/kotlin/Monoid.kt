e05cdb1d8a6b2fc9070dfc8ea46b564d8caff347
Mike Sperber <sperber@deinprogramm.de>
2024-07-22 16:11:08 +0200

    Update.

--- src/main/kotlin/Monoid.kt
+++ src/main/kotlin/Monoid.kt
@@ -2,6 +2,8 @@ interface Monoid<A>: Semigroup<A> {
     fun neutral(): A
 }
 
-class ListMonoid<A>: ListSemigroup<A>() {
-    fun neutral() = Empty
+class ListMonoid<A>: ListSemigroup<A>(), Monoid<List<A>> {
+    override fun neutral() = Empty
 }
+
+fun <A> listMonoid() = ListMonoid<A>()
