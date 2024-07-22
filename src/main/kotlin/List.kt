6664a0c012a3946885602db826929b6a251038b3
Mike Sperber <sperber@deinprogramm.de>
2024-07-22 13:18:39 +0200

    Update.

--- src/main/kotlin/List.kt
+++ src/main/kotlin/List.kt
@@ -6,19 +6,31 @@ Liste ist eins der folgenden:
                                                         Selbstbezug
  */
 
-sealed interface List
+sealed interface List<out A> {
+}
 
-object Empty : List
+// extension method
+fun <A> List<A>.filter(p: (A) -> Boolean): List<A> =
+    when (this) {
+        is Empty -> Empty
+        is Cons ->
+            if (p(this.first))
+               Cons(this.first,  this.rest.filter(p))
+            else
+                this.rest.filter(p)
+    }
+
+object Empty : List<Nothing>
 
 /* Eine Cons-Liste besteht aus:
  - erstes Element -UND-
  - Rest-Liste
  */
 
-data class Cons(val first: Int, val rest: List) : List
+data class Cons<A>(val first: A, val rest: List<A>) : List<A>
 
-// 1elementige Liste: 5
-val list1 = Cons(5, Empty)
+// 1elementige Liste: 5:Int
+val list1: List<Int> = Cons(5, Empty)
 // 2elementige Liste: 2 5
 val list2 = Cons(2, Cons(5, Empty))
 // 3elementige Liste: 7 2 5
@@ -26,7 +38,7 @@ val list3 = Cons(7, list2)
 val list4 = Cons(10, list3)
 
 // Elemente einer Liste addieren
-fun listSum(list: List): Int =
+fun listSum(list: List<Int>): Int =
     when (list) {
         is Empty -> 0 // neutrales Element von +
         is Cons ->
@@ -34,8 +46,10 @@ fun listSum(list: List): Int =
                     listSum(list.rest) // Selbstbezug
     }
 
+// val sum1 = listSum(list1)
+
 // Elemente einer Liste multiplizieren
-fun listProduct(list: List): Int =
+fun listProduct(list: List<Int>): Int =
     when (list) {
         is Empty -> 1 // neutrales Element von *
         is Cons ->
@@ -43,7 +57,43 @@ fun listProduct(list: List): Int =
                 listProduct(list.rest)
     }
 
+val highway = Cons(dillo1, Cons(dillo2, Cons(snake1, Empty)))
+
 // neutrales Element:
 // x + 0 = 0 + x = x
 // x * 1 = 1 * x = x
 // x * 1 = 1 * x = x
+
+fun isEven(n: Int): Boolean =
+    n % 2 == 0
+
+fun listEvens(list: List<Int>): List<Int> =
+    when (list) {
+        is Empty -> Empty
+        is Cons ->
+            if (isEven(list.first))
+                Cons(list.first, listEvens(list.rest))
+            else
+                listEvens(list.rest)
+    }
+
+// Funktionen höherer Ordnung
+// "higher-order functions"
+fun <A>listFilter(list: List<A>, p: (A) -> Boolean): List<A> =
+    when (list) {
+        is Empty -> Empty
+        is Cons ->
+            if (p(list.first))
+                Cons(list.first, listFilter(list.rest, p))
+            else
+                listFilter(list.rest, p)
+    }
+
+// syntaktischer Zucker:
+// letzter Argument ist eine Funktion
+val list4Evens = listFilter(list4) { n -> isEven(n) }
+
+val dillos = Cons(dillo1, Cons(dillo2, Empty))
+
+// Eine Funktion auf alle Elemente einer Liste anwenden
+// fun listMap()
--- src/main/kotlin/Shape.kt
+++ src/main/kotlin/Shape.kt
@@ -8,3 +8,33 @@
 // 1. Datenmodellierung
 // 2. Funktion, die ermittelt, ob ein gegebener Punkt
 //    innerhalb oder außerhalb einer Figur ist
+
+data class Point(val x: Double, val y: Double)
+
+sealed interface Shape
+
+data class Circle(val center: Point, val radius: Double): Shape
+
+data class Square(val llCorner: Point, val sideLength: Double): Shape
+
+data class Overlay(val shape1: Shape, val shape2: Shape): Shape
+
+fun distance(point1: Point, point2: Point): Double {
+    val dx = point1.x - point2.x
+    val dy = point1.y - point2.y
+    return Math.sqrt(dx * dx + dy * dy)
+}
+
+
+fun isIn(point: Point, shape: Shape): Boolean =
+    when (shape) {
+        is Circle ->
+            distance(point, shape.center) <= shape.radius
+        is Square -> {
+            val ll = shape.llCorner
+            (ll.x <= point.x) && (point.x <= ll.x + shape.sideLength) &&
+                    (ll.y <= point.y) && (point.y <= ll.y + shape.sideLength)
+        }
+        is Overlay ->
+            isIn(point, shape.shape1) || isIn(point, shape.shape2)
+    }
