3e8d130b2bf7be201dc8dd911595502a8515611a
Mike Sperber <sperber@deinprogramm.de>
2024-07-22 09:28:45 +0200

    Update.

--- src/main/kotlin/Animal.kt
+++ src/main/kotlin/Animal.kt
@@ -5,5 +5,12 @@ Gürteltier hat folgende Eigenschaften:
 - (lebendig ODER tot) - UND -
 - Gewicht
 zusammengesetzte Daten
+
+Lebendigkeit ist eins der folgenden:
+- lebendig -ODER-
+- tot
+Fallunterscheidung
+hier: Aufzählung
  */
 
+enum class Liveness { DEAD, ALIVE }
