e355b9f056c7215f1430412e2f40610af25005f3
Mike Sperber <sperber@deinprogramm.de>
2024-07-22 10:21:54 +0200

    Update.

--- src/main/kotlin/List.kt
+++ src/main/kotlin/List.kt
@@ -23,3 +23,13 @@ val list1 = Cons(5, Empty)
 val list2 = Cons(2, Cons(5, Empty))
 // 3elementige Liste: 7 2 5
 val list3 = Cons(7, list2)
+val list4 = Cons(10, list3)
+
+// Elemente einer Liste addieren
+fun listSum(list: List): Int =
+    when (list) {
+        is Empty -> 0
+        is Cons ->
+            list.first +
+                    listSum(list.rest) // Selbstbezug
+    }
