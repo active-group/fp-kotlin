c9c2430f86897b23d8c7b4374274884cb9140db9
Mike Sperber <sperber@deinprogramm.de>
2024-07-22 11:12:15 +0200

    Update.

--- src/main/kotlin/ShowerProduct.kt
+++ src/main/kotlin/ShowerProduct.kt
@@ -1,9 +1,36 @@
-// Duschprodukte
+// Duschprodukt:
+// - Seife -ODER-
+// - Shampoo -ODER-
+// - Duschgel
+sealed interface ShowerProduct
 
-// Seife
+// Seife hat folgende Eigenschaften:
+// - pH-Wert
+data class Soap(val pH: Double): ShowerProduct
 
-// Shampoo
+// Shampoo hat folgende Eigenschaften:
+// - Haartyp (Schuppen, fettig, normal)
+enum class Hairtype { DANDRUFF, OILY, NORMAL }
+
+data class Shampoo(val hairtype: Hairtype): ShowerProduct
 
 // Duschgel besteht aus:
-// - Seife
+// - Seife -UND-
 // - Shampoo
+// data class Showergel(val soap: Soap, val shampoo: Shampoo)
+//    : ShowerProduct
+
+// Kombinator
+// "closure of operations"
+data class Mixture(val product1: ShowerProduct, // Selbstbezug
+    val product2: ShowerProduct): ShowerProduct
+
+// Seifentanteil eines Duschproducts
+fun soapProportion(product: ShowerProduct): Double =
+    when (product) {
+        is Soap -> 1.0
+        is Shampoo -> 0.0
+        is Mixture ->
+            (soapProportion(product.product1) +
+                soapProportion(product.product2)) / 2.0
+    }
