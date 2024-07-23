0e5f3adbe6546ef337cf9b2dac3f8805cad4441c
Mike Sperber <sperber@deinprogramm.de>
2024-07-23 13:44:20 +0200

    Update.

--- src/main/kotlin/Validation.kt
+++ src/main/kotlin/Validation.kt
@@ -6,12 +6,28 @@ Yaron Minsky: "Make illegal states unrepresentable."
 
 data class Person(val name: String, val age: Int)
 
-fun makePerson(name: String, age: Int): Option<Person> =
-    if (age >= 0)
-        Some(Person(name, age))
-    else
+fun makePersonOptional(name: String, age: Int): Option<Person> =
+    if (name == "")
         None
+    else
+        if (age >= 0)
+            Some(Person(name, age))
+        else
+            None
 
-sealed interface Validation<A>
-data class Valid<A>(val value: A)
-data class Invalid(val errors: List<String>)
+sealed interface Validation<out A>
+data class Valid<A>(val value: A): Validation<A>
+data class Invalid(val errors: List<String>): Validation<Nothing>
+
+fun makePerson(name: String, age: Int): Validation<Person> =
+    if (name == "")
+        if (age >= 0)
+            Invalid(Cons("Name darf nicht leer sein", Empty))
+        else
+            Invalid(Cons("Name darf nicht leer sein", Cons("Alter darf nicht negativ sein", Empty)))
+    else
+        if (age >= 0)
+            Valid(Person(name, age))
+        else
+            Invalid(Cons("Alter darf nicht negativ sein", Empty))
+
